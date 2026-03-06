fca8ca9a60d8f602ecd41ebe82a0c2d9bb2adc4a
Jason Forbus <jifor1030@gmail.com>
2026-03-05 22:49:25 -0600

    Model using rolling average and stdev

--- src/features.py
+++ src/features.py
@@ -9,9 +9,47 @@ def add_last_finish(df):
     Incoming dataframe from get_season_results() looks like
     index|DriverNumber|RoundNumber|Position
     '''
-    df["last_finish"] = (
+    df["LastFinish"] = (
         df.groupby("DriverNumber")["Position"]
         .shift(1)
     )
+
+    return df
+
+def add_rolling_3_avg(df):
+    '''
+    Add a column with a rolling 3 race position average to dataframe
+
+    Incoming dataframe looks like
+    index|DriverNumber|RoundNumber|Position|...
+    '''
+
+    df = df.sort_values(["DriverNumber", "RoundNumber"])
+
+    # Compute the rolling average of last 3 finish positions. This is from ChatGPT lol :p
+    # .fillna(df['Position'] fill the NaN from the first race of each season with current position. Not the best method but works okay
+    df['RollingAverage3'] = df.groupby('DriverNumber')['Position'].transform(lambda x: x.shift(1).rolling(3, min_periods=1).mean()).fillna(df['Position'])
+
+    return df
+
+def add_3_race_consistency(df):
+    '''
+    Add a column with a rolling 3 race standard deviation of Position
+
+    Incoming dataframe looks like
+    index|DriverNumber|RoundNumber|Position|...
+    '''
+
+    df = df.sort_values(['DriverNumber', 'RoundNumber'])
+
+    df['RollingStd3'] = df.groupby('DriverNumber')['Position'].transform(lambda x: x.shift(1).rolling(3, min_periods=2).std()).fillna(0)
+    
+    return df
+
+def add_rolling_5_avg(df):
+    return 0
+
+def add_results_features(df):
+
 
     return df
