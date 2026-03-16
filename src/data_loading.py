ab8459cf4924a086596ff974020c09344f2875ec
Jason F <jifor1030@gmail.com>
2026-03-16 19:57:31 -0400

    Cleaned up data_loading to split up functions

--- src/data_loading.py
+++ src/data_loading.py
@@ -12,99 +12,84 @@ def get_events_in_season(year):
 
     return len(schedule.index) 
 
-
-def get_season_results(season):
+def get_loaded_session(season_year, round_num, session_type='R'):
     '''
-    Function that will get previous race results, put them into a dataframe, and return the dataframe of results.
-
-    Returns dataframe:
-    DriverNumber|RoundNumber|TeamName|Position
-
-    Things to consider:
-        - need a way to handle dsqs and retires. dsq is difficult because in recent seasons dsqs did very well in the race
-            - Ferrari in China 2025, Russell in Belgium 2024, Mclaren in 2025 in Las Vegas
-    '''
-    ff1.Cache.enable_cache(cache.cache_path) # these can be removed once I have a working main.py
-
-    session_type = 'R'
-
-    # get the number of events in a season that results can be pulled for
-    no_events = get_events_in_season(season)
-    rounds = range(1, no_events+1)
-    # rounds = range(1,5)
-    all_results = []
-
-    for round in rounds:
-        session = ff1.get_session(season, round, session_type)
-        session.load()
-
-        result = session.results['ClassifiedPosition']
-        team = session.results['TeamName']
-        result.index = result.index.astype(int)
-        # Replace 'R', 'D', 'W' with 20
-        result = pd.to_numeric(result, errors="coerce").fillna(20)
-
-        race_df = pd.DataFrame({
-            "DriverNumber": result.index,
-            "RoundNumber": round,
-            "TeamName": team,
-            "Position": result.values
-        })
-
-        race_df = race_df.sort_values('DriverNumber')
-
-        all_results.append(race_df)
-
-    results = pd.concat(all_results, ignore_index=True)
-    return results
-
-def get_season_qual_results(season):
-    '''
-    Gets qualifying results for a season and returns them in a dataframe with Driver Number and Round Number
+    Return loaded session
     '''
     ff1.Cache.enable_cache(cache.cache_path)
 
-    session_type = 'Q'
+    session = ff1.get_session(season_year, round_num, session_type)
+    session.load()
 
-    no_events = get_events_in_season(season)
-    rounds = range(1, no_events+1)
-    
-    all_results = []
+    return session
 
-    for round in rounds:
-        session = ff1.get_session(season, round, session_type)
-        session.load()
-
-        result = session.results['Position']
-        result.index = result.index.astype(int)
-        # Fill missing values with 20
-        result = pd.to_numeric(result, errors="coerce").fillna(20)
-
-        qual_df = pd.DataFrame({
-            "DriverNumber": result.index,
-            "RoundNumber": round,
-            "QualiPosition": result.values
-        })
-
-        qual_df = qual_df.sort_values('DriverNumber')
-
-        all_results.append(qual_df)
-
-    results = pd.concat(all_results, ignore_index=True)
-
-    return results
-
-def get_event_qual_times(session_year, round_number):
+def get_race_results(season_year=None, round_number=None, session=None):
     '''
-    Get the fastest times for each driver in a qualifying session
+    Returns DataFrame with results from a race session. Can take in either session information or a loaded session.
+
+    DataFrame:
+    DriverNumber|RoundNumber|TeamName|Position
+    '''
+    if session is None:
+        if season_year is None or round_number is None:
+            raise ValueError('Need either a loaded session or session information')
+        session = get_loaded_session(season_year, round_number, session_type='R')
+
+    result = session.results['ClassifiedPosition']
+    team = session.results['TeamName']
+    result.index = result.index.astype(int)
+    # Replace 'R', 'D', 'W' with 20
+    result = pd.to_numeric(result, errors="coerce").fillna(20) # temporary, need a way to fix DSQs
+
+    race_df = pd.DataFrame({
+        "DriverNumber": result.index,
+        "RoundNumber": session.event['RoundNumber'],
+        "TeamName": team,
+        "Position": result.values
+    })
+
+    race_df = race_df.sort_values('DriverNumber')
+
+    return race_df
+
+def get_quali_results(season_year=None, round_number=None, session=None):
+    '''
+    Returns DataFrame with results from a qualifying session. Can take in either session information or a loaded session.
+
+    DataFrame:
+    DriverNumber|RoundNumber|QualiPosition 
+    '''
+    if session is None:
+        if season_year is None or round_number is None:
+            raise ValueError('Need either a loaded session or session information')
+        session = get_loaded_session(season_year, round_number, session_type='Q')   
+
+    result = session.results['Position']
+    result.index = result.index.astype(int)
+    # Fill missing values with 20 because no quali result means they would start at the end
+    result = pd.to_numeric(result, errors="coerce").fillna(20)
+
+    qual_df = pd.DataFrame({
+        "DriverNumber": result.index,
+        "RoundNumber": session.event['RoundNumber'],
+        "QualiPosition": result.values
+    })
+
+    qual_df = qual_df.sort_values('DriverNumber')
+
+    return qual_df
+
+def get_quali_times(season_year=None, round_number=None, session=None):
+    '''
+    Get the fastest times for each driver in a qualifying session. Can take either session information or a loaded session.
 
     Returns
     DriverNumber|RoundNumber|FastestTime
     '''
-    ff1.Cache.enable_cache(cache.cache_path)
-
-    session = ff1.get_session(session_year, round_number, 'Q')
-    session.load()
+    if session is None:
+        if season_year is None or round_number is None:
+            raise ValueError('Need either a loaded session or session information')
+        session = get_loaded_session(season_year, round_number, session_type='Q')
 
     rows = []
     for driver in session.drivers:
@@ -122,28 +107,6 @@ def get_event_qual_times(session_year, round_number):
     qual_times = pd.DataFrame(rows)
     qual_times['FastestTime'] = qual_times['FastestTime'].dt.total_seconds()
     return qual_times
-
-def get_season_qual_times(season):
-    '''
-    Get the fastest time for each driver for every qualifying session in a season
-
-    Returns df like
-    DriverNumber|RoundNumber|FastestTime
-    '''
-
-    # Cache is enabled by get_event_qual_times()
-
-    no_events = get_events_in_season(season)
-    rounds = range(1, no_events+1)
-
-    all_results = []
-    for round in rounds:
-        times = get_event_qual_times(season, round)
-        times = times.sort_values('DriverNumber')
-        all_results.append(times)
-
-    results = pd.concat(all_results, ignore_index=True)
-    return results 
 
 # def get_rain_in_race(season):
 #     '''
@@ -176,6 +139,36 @@ def get_season_qual_times(season):
 
 #     return season_rain
 
+def get_data_for_season(season):
+    '''
+    Returns DataFrame with results for a whole season.
+
+    DataFrame:
+    DriverNumber|Season|RoundNumber|TeamName|Position|QualiPosition|FastestTime
+    '''   
+    events = get_events_in_season(season)
+    rounds = range(1, events+1)
+
+    all_results = []
+    for round in rounds:
+        race = get_loaded_session(season, round, 'R')
+        quali = get_loaded_session(season, round, 'Q')
+        race.load()
+        quali.load()
+
+        quali_res = get_quali_results(session=quali) # DriverNumber|RoundNumber|QualiPosition
+        quali_times = get_quali_times(session=quali) # DriverNumber|RoundNumber|FastestTime
+        race_res = get_race_results(session=race) # DriverNumber|RoundNumber|TeamName|Position
+
+        res_df1 = pd.merge(race_res, quali_res, on=['DriverNumber', 'RoundNumber'], how='left')
+        res_df2 = pd.merge(res_df1, quali_times, on=['DriverNumber', 'RoundNumber'], how='left')
+
+        all_results.append(res_df2)
+
+    results = pd.concat(all_results, ignore_index=True)
+
+    return results
+
 def make_data_dataframe(seasons: list):
     '''
     Make a dataframe of all the results I want for the model
@@ -183,20 +176,12 @@ def make_data_dataframe(seasons: list):
     Output:
     DriverNumber|Season|RoundNumber|TeamName|Position|QualiPosition|FastestTime
     '''
-
-    ff1.Cache.enable_cache(cache.cache_path)
-
     results = []
     for season in seasons:
-        qual_res = get_season_qual_results(season) # DriverNumber|RoundNumber|QualiPosition
-        qual_times = get_season_qual_times(season) # DriverNumber|RoundNumber|FastestTime
-        race_res = get_season_results(season) # DriverNumber|RoundNumber|TeamName|Position
+        season_res = get_data_for_season(season)
+        season_res = season_res.insert(1,'Season', season)
 
-        res_df1 = pd.merge(race_res, qual_res, on=['DriverNumber', 'RoundNumber'], how='left')
-        res_df2 = pd.merge(res_df1, qual_times, on=['DriverNumber', 'RoundNumber'], how='left')
-        res_df2.insert(1, 'Season', season)
-
-        results.append(res_df2)
+        results.append(season_res)
 
     df = pd.concat(results, ignore_index=True)
 
